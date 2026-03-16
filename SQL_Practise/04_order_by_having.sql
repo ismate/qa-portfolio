e6e091c42f08dc0e52f38fbbeffb4fa566c3c1bc
ismate <81077694+ismate@users.noreply.github.com>
2026-03-16 20:54:35 +0300

    Update 04_order_by_having.sql

--- SQL_Practise/04_order_by_having.sql
+++ SQL_Practise/04_order_by_having.sql
@@ -1,1 +1,24 @@
+-- Задание 4: INSERT / UPDATE / DELETE
+-- Цель: научиться добавлять, изменять и удалять данные в таблицах
 
+-- Таблицы для примера:
+-- users (id, name, age, city)
+-- orders (id, user_id, amount)
+
+
+-- 1. Добавление нового пользователя
+
+INSERT INTO users (id, name, age, city)
+VALUES (6, 'Ivan', 28, 'Saint-P');
+
+
+-- 2. Изменение данных существующего пользователя
+
+UPDATE users
+SET city = 'Saint-P', age = 35
+WHERE id = 4;
+
+
+-- 3. Удаление пользователя из таблицы
+DELETE FROM users
+WHERE id = 5;
