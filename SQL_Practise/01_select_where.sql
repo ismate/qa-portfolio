d0511ad000fa68126a9c6dc567f9529f2a642a93
ismate <81077694+ismate@users.noreply.github.com>
2026-03-16 19:23:30 +0300

    Update 01_select_where.sql

--- SQL_Practise/01_select_where.sql
+++ SQL_Practise/01_select_where.sql
@@ -1,1 +1,14 @@
+-- Задание 1: SELECT + WHERE
+-- Цель: научиться выбирать данные с фильтрацией по условиям
 
+-- Таблица: users (id, name, age, city, email)
+
+-- 1. Выбрать всех пользователей, которые живут в Москве
+SELECT * 
+FROM users 
+WHERE city = 'Moscow';
+
+-- 2. Выбрать имя и email всех пользователей старше 25 лет
+SELECT name, email 
+FROM users 
+WHERE age > 25;
